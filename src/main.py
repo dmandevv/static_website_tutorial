badf5e47e34f7e9c670732a8d34fb0e0c9757fb7
dmandevv <dmandevv@hotmail.com>
2025-02-17 16:55:21 -0800

    moved root git folder

--- src/main.py
+++ src/main.py
@@ -2,12 +2,24 @@ from textnode import *
 from htmlnode import *
 
 import os
+import shutil
 
 def main():
-    pass
+    copy_files_recursive("/home/ar0na/workspace/github.com/dmandevv/static_website_tutorial/static",
+                         "/home/ar0na/workspace/github.com/dmandevv/static_website_tutorial/public")
+
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
 
 if __name__ == '__main__':
     main()
-
-def copy_to_directory(from_here, to_here):
-    if os.path.exists
